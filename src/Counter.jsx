f864e37c8b42ef8c2e6743bddb898a7eba6d3b9e
shrisivaclg <shrisivaclg@gmail.com>
2024-12-10 20:39:11 -0500

    Updated code with new features or bug fixes2

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -4,36 +4,27 @@ import './Counter.css';
 class Counter extends Component {
     constructor(props) {
         super(props);
-        this.state = { count: 0 };
+        this.state = { count: 5 }; // Start from 5
     }
 
     increment = () => {
         this.setState({ count: this.state.count + 1 });
     };
 
-    decrement = () => {
-        if (this.state.count > 0) {
-            this.setState({ count: this.state.count - 1 });
-        }
-    };
-
-    reset = () => {
-        this.setState({ count: 0 });
-    };
-
     render() {
         return (
             <div className="counter-container">
-                <h1>{this.state.count}</h1>
-                <button className="counter-button" onClick={this.increment}>
-                    Increment
-                </button>
-                <button className="counter-button" onClick={this.decrement}>
-                    Decrement
-                </button>
-                <button className="counter-button reset" onClick={this.reset}>
-                    Reset
-                </button>
+                <div className="counter-header">
+                    <h1>Counter</h1>
+                </div>
+                <div className="counter-display">
+                    <h2>{this.state.count}</h2>
+                </div>
+                <div className="counter-controls">
+                    <button className="counter-button" onClick={this.increment}>
+                        Increment
+                    </button>
+                </div>
             </div>
         );
     }
